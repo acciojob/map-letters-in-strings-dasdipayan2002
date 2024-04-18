f19bc8781e124f08839cd7c252d9676eaece7e67
Dipayan Das <105163490+dasdipayan2002@users.noreply.github.com>
2024-04-18 11:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function mapLetters(str) {
+	let obj = {};
+	for(let i = 0; i < str.length; i++){
+		let t = str[i];
+		if(obj[t] == undefined){
+			obj[t] = [i];
+		}
+		else{
+			let arr = obj[t];
+			arr.push(i);
+		}
+	}
+	return obj;
+}
